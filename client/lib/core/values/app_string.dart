8323545b7507a371ed9eb83f23687f5dc2c16d83
belema <tamunobelemaachego@gmail.com>
2024-12-08 15:10:38 +0100

    stash

--- client/lib/core/values/app_string.dart
+++ client/lib/core/values/app_string.dart
@@ -1,13 +1,17 @@
 class AppStrings {
   final _isDevMode = false;
+  final _isRealDevice = true;
+
   String get appTitle => "Igbigi";
 
   String get success => "Success";
 
-  String get apiBaseUrl => _isDevMode ? emulatorAddress : prodUrl;
+  String get apiBaseUrl =>
+      _isDevMode ? (_isRealDevice ? realDevUrl : emulatorAddress) : prodUrl;
 
   String get emulatorAddress => 'http://10.0.2.2:$port';
   String get prodUrl => 'https://tdex-hackerton.vercel.app';
+  String get realDevUrl => 'https://l6rfzblz-3500.uks1.devtunnels.ms/';
   String get port => '3500';
 
   final errorText = ErrorText();
